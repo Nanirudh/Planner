a3a25a86467cbeeeb1e222bbdc0212b6e5b7ab96
Nanirudh <nandurianirudh28@gmail.com>
2018-06-20 21:58:33 +0530

    MainPage

---
+++ MainPage.inc.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Site Manager</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale-1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<style>
+ h2{
+   color:red;
+   text-align:center;
+ }
+ p{
+   font-family:Comic Sans Ms;
+   text-align:center;
+ }
+ 
+ .par{
+   text-align:center;
+ }
+</style>
+<body>
+<nav class="navbar navbar-default">
+  <div class= "container-fluid">
+                <a class="navbar-brand" href="http://localhost/proj1/">
+                <img src="https://cdn.dribbble.com/users/60166/screenshots/3488998/m_logo_1x.jpg" width="60" height="40">
+                </a>
+                <a href= "http://localhost/project1/MainPage.inc.php" class="navbar-brand">Site Manager</a>
+                <ul class="nav navbar-nav navbar-right">
+                   <li><a href="http://localhost/project1/loginform.inc.php"><code>Log-in</code></a></li>
+                   <li><a href="http://localhost/project1/Regform.php"><code>Register</code></a></li>
+                </ul>
+
+        </div>
+</nav>
+
+<img src="https://cdn.dribbble.com/users/60166/screenshots/3488998/m_logo_1x.jpg" class="img-responsive center-block">
+                </a>
+
+ <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4" style="background-color:yellow">
+      <h2>Manage</h2>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvcJU5rSJzpgrm7Gz-F22_yO0l-uR1gOdcy4tFITmerrrkQg-5cA" class="img-responsive center-block">
+      <p>Manage your bookmarks,files along with short description in a clean,concise way.</p>
+      </div>
+ </div>
+
+
+
+</body>
+</html>
